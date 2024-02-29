8b6572d2ca573aeeb255a8868564f0412ae75a8d
stringthread <string.thread@gmail.com>
2024-02-29 17:21:39 +0900

    feat: Add type annotations for SafeRepository methods

--- src/repository/SafeRepository.ts
+++ src/repository/SafeRepository.ts
@@ -1,12 +1,58 @@
 import { StrictEntity } from "@/entity/StrictEntity";
 import { FindOptionsPath } from "@/options/FindOptions";
-import { FindManyOptions, ObjectLiteral, Repository } from "typeorm";
+import {
+  FindManyOptions,
+  FindOneOptions,
+  FindOptionsWhere,
+  ObjectId,
+  ObjectLiteral,
+  Repository,
+} from "typeorm";
 
 export type SafeRepository<Entity extends ObjectLiteral> = Omit<
   Repository<Entity>,
-  "find"
-> & {
+  keyof SafeRepositoryOverrides<Entity>
+> &
+  SafeRepositoryOverrides<Entity>;
+
+interface SafeRepositoryOverrides<Entity extends ObjectLiteral> {
   find<Options extends FindManyOptions<Entity> = never>(
     options?: Options
+  ): Promise<StrictEntity<Entity, FindOptionsPath<Entity, Options>>[]>;
+  findBy(
+    options?: FindOptionsWhere<Entity> | FindOptionsWhere<Entity>[]
+  ): Promise<StrictEntity<Entity>[]>;
+  findAndCount<Options extends FindManyOptions<Entity> = never>(
+    options?: Options
+  ): Promise<
+    [StrictEntity<Entity, FindOptionsPath<Entity, Options>>[], number]
+  >;
+  findAndCountBy(
+    options?: FindOptionsWhere<Entity> | FindOptionsWhere<Entity>[]
+  ): Promise<[StrictEntity<Entity>[], number]>;
+  /**
+   * @deprecated use `findBy` method instead in conjunction with `In` operator
+   */
+  findByIds(ids: any[]): Promise<StrictEntity<Entity>[]>; // eslint-disable-line @typescript-eslint/no-explicit-any
+  findOne<Options extends FindOneOptions<Entity> = never>(
+    options?: Options
+  ): Promise<StrictEntity<Entity, FindOptionsPath<Entity, Options>> | null>;
+  findOneBy(
+    options?: FindOptionsWhere<Entity> | FindOptionsWhere<Entity>[]
+  ): Promise<StrictEntity<Entity> | null>;
+  /**
+   * @deprecated use `findOneBy` method instead in conjunction with `In` operator
+   */
+  findOneById(
+    id: number | string | Date | ObjectId
+  ): Promise<StrictEntity<Entity> | null>;
+  findOneOrFail<Options extends FindOneOptions<Entity> = never>(
+    options?: Options
   ): Promise<StrictEntity<Entity, FindOptionsPath<Entity, Options>>>;
-};
+  findOneByOrFail(
+    options?: FindOptionsWhere<Entity> | FindOptionsWhere<Entity>[]
+  ): Promise<StrictEntity<Entity>>;
+  extend<CustomRepository>(
+    customs: CustomRepository & ThisType<this & CustomRepository>
+  ): this & CustomRepository;
+}
